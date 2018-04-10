450317a99a14c4b058eaae07fb1a93106935f6e0
Artem <southred@yandex.ru>
2018-04-10 23:38:20 +0300

    better

--- src/EvalDemo.hs
+++ src/EvalDemo.hs
@@ -1,8 +1,15 @@
 module EvalDemo (run) where
 
-run :: IO ()
-run = print $ show (factorial 5)
+import System.Environment
 
-factorial :: Int -> Int
+run :: IO ()
+run = do
+   args <- getArgs
+   let
+      ints = [2..100]
+      factorials = fmap factorial ints
+   print $ show factorials
+
+factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
