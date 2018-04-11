5629b8b38c91da9854a0d109e62f49a6b072bec2
Artem <southred@yandex.ru>
2018-04-11 22:57:59 +0300

    par map

--- src/EvalDemo.hs
+++ src/EvalDemo.hs
@@ -2,13 +2,27 @@ module EvalDemo
   ( run
   ) where
 
-import System.Environment
+import Control.Parallel.Strategies
 
 run :: IO ()
 run = do
   let ints = [1 .. 50000]
-      calculated = filter (>50) (fmap numberOfDivisors ints)
+      calculated = filter (>50) (calculateParallel ints)
   print $ length calculated
+
+calculateSequentially :: [Int] -> [Int]
+calculateSequentially ints = fmap numberOfDivisors ints
+
+calculateParallel :: [Int] -> [Int]
+calculateParallel ints = runEval $ myParMap numberOfDivisors ints
+
+myParMap :: (a -> b) -> [a] -> Eval [b]
+myParMap f [] = return []
+myParMap f (x:xs) = do
+            x' <- rpar (f x)
+            xs' <- myParMap f xs
+            return (x':xs')
+
 
 numberOfDivisors :: Int -> Int
 numberOfDivisors n = length $ divisors n
