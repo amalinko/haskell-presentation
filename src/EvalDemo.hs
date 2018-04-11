fc259e369aaf60c04853282529e2f312ebfda97f
Artem <southred@yandex.ru>
2018-04-11 22:12:46 +0300

    divisors

--- src/EvalDemo.hs
+++ src/EvalDemo.hs
@@ -1,15 +1,19 @@
-module EvalDemo (run) where
+module EvalDemo
+  ( run
+  ) where
 
 import System.Environment
 
 run :: IO ()
 run = do
-   args <- getArgs
-   let
-      ints = [2..100]
-      factorials = fmap factorial ints
-   print $ show factorials
+  let ints = [2 .. 1000]
+      calculated = fmap numberOfDivisors ints
+  print $ show calculated
 
-factorial :: Integer -> Integer
-factorial 0 = 1
-factorial n = n * factorial (n - 1)
+numberOfDivisors :: Int -> Int
+numberOfDivisors n = length $ divisors n
+
+divisors :: Int -> [Int]
+divisors n = 1 : filter isDivisor [2 .. n `div` 2]
+  where
+    isDivisor x = rem n x == 0
